055c971c3192fa60086520db84941c94a7db662d
Diony junior Diaz <dionyjunior11@gmail.com>
2026-01-27 13:14:35 -0400

    Configure CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from app.api.auth import auth_bp
 from app.api.ecf import ecf_bp
+from flask_cors import CORS
+
 def create_app(config_class):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    
+    # Initialize CORS
+    CORS(app)
 
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(ecf_bp, url_prefix='/ecf')
